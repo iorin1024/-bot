eed0050a8acf7dfb2c992bccc061d7c5eca78b18
iorin1024 <e233tkd08@gmail.com>
2024-12-19 16:54:07 +0900

    1

--- sent.py
+++ sent.py
@@ -2,6 +2,8 @@ import discord
 from datetime import datetime
 from discord.ext import tasks 
 import asyncio
+import os
+from keep_alive import keep_alive
 
 TOKEN = "MTMwNTQ4MzkzMTAzMzc5NjYyOQ.Gh8MPC.ll6KRL67dVMLBP7rciVAiaVKQ7XkZQMEIWN1C8"
 CHANNEL_ID = 1318500728691752980
@@ -25,5 +27,9 @@ async def on_ready():
     print('on ready')
     loop.start()
 
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+# Web サーバの立ち上げ
+keep_alive()
 client.run(TOKEN)
 
