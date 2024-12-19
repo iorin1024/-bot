b9da1081347f5b8b2f4f9a92d9b6654c4fdb5f29
iorin24 <119677020+iorin1024@users.noreply.github.com>
2024-12-19 16:38:01 +0900

    add file
    nothing

---
+++ sent.py
@@ -0,0 +1,29 @@
+import discord
+from datetime import datetime
+from discord.ext import tasks 
+import asyncio
+
+TOKEN = "MTMwNTQ4MzkzMTAzMzc5NjYyOQ.Gh8MPC.ll6KRL67dVMLBP7rciVAiaVKQ7XkZQMEIWN1C8"
+CHANNEL_ID = 1318500728691752980
+
+client = discord.Client(intents=discord.Intents.all())
+
+datalist = [
+'8:30',
+'20:30',
+]
+
+@tasks.loop(seconds=60)
+async def loop():
+    now = datetime.now().strftime('%H:%M')
+    if now in datalist :
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('ﾊﾁｼﾞﾊｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧｧﾝﾝ!!!!!!!!!!!!')
+
+@client.event
+async def on_ready():
+    print('on ready')
+    loop.start()
+
+client.run(TOKEN)
+
