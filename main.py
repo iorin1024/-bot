532cd9ddd9fed694ee8e06b895026b33c0b15bad
iorin1024 <e233tkd08@gmail.com>
2025-01-09 17:07:31 +0900

    #

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import discord
-from datetime import datetime
+from discord import app_commands,ui
 from discord.ext import tasks 
 import asyncio
 import os
 from keep_alive import keep_alive
 from dotenv import load_dotenv
 
-CHANNEL_ID = 1318500728691752980
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+SERVER_ID = os.getenv("SERVER_ID")
 
 client = discord.Client(intents=discord.Intents.all())
+tree = app_commands.CommandTree(client)
 
 datalist = [
 '8:30',
@@ -25,7 +27,12 @@ async def loop():
 @client.event
 async def on_ready():
     print('on ready')
+    await tree.sync(guild=discord.Object(id={SERVER_ID}))
     loop.start()
+
+@tree.command(name="怪文書生成",description="ルイズ構文を使用した怪文書を簡単に作れます")
+@discord.app_commands.guilds({SERVER_ID})
+async def kaibun(interaction : discord.interaction):
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
