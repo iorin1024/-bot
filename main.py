055a367ea84aa2b8221196214dc664de6b33e34a
iorin24 <119677020+iorin1024@users.noreply.github.com>
2025-02-12 22:09:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ JST = timezone(timedelta(hours=+9), "JST")
 
 times = [
     time(hour=8, minute=30, tzinfo=JST),
-    time(hour=22, minute=05, tzinfo=JST)
+    time(hour=22, minute=12, tzinfo=JST)
 ]
 
 @tasks.loop(time=times)
