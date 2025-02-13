cea81d156f718990b08d1ced25d49130767e58c7
iorin24 <119677020+iorin1024@users.noreply.github.com>
2025-02-13 12:44:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def loop():
 async def on_ready():
     print('on ready')
     loop.start()
-    await tree.sync(guild=discord.Object(id={1127225622104641651}))
+    await tree.sync(guild=discord.Object(id={"1127225622104641651"}))
     print("Synced slash commands")
 
 @tree.command(name="stable", description="日本の政治を安定させます！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！れ")
